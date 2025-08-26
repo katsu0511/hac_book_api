6f970e3d3b915e2a7791aad53a569e13efb7f6cd
Katsuya <haradakatsuya190511@gmail.com>
2025-08-26 14:57:42 -0700

    created db connection setting

--- src/main/java/com/haradakatsuya190511/HacBookApiApplication.java
+++ src/main/java/com/haradakatsuya190511/HacBookApiApplication.java
@@ -2,11 +2,17 @@ package com.haradakatsuya190511;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class HacBookApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+	    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+	    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(HacBookApiApplication.class, args);
 	}
 
