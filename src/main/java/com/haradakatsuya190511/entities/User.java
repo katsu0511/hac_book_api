10df098be718c761a617567fb1fc8fefa6add051
Katsuya <haradakatsuya190511@gmail.com>
2025-08-29 14:12:07 -0700

    created User entity

---
+++ src/main/java/com/haradakatsuya190511/entities/User.java
@@ -0,0 +1,126 @@
+package com.haradakatsuya190511.entities;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 32, nullable = false)
+	@NotBlank
+	@Size(max = 32)
+	private String name;
+	
+	@Column(length = 100, nullable = false, unique = true)
+	@Email
+	@NotBlank
+	@Size(max = 100)
+	private String email;
+	
+	@Column(length = 255, nullable = false)
+	private String password;
+	
+	@Column(insertable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+	private Setting setting;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	@JsonIgnore
+	private List<Category> categories = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	@JsonIgnore
+	private List<Transaction> transactions = new ArrayList<>();
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	
+	public Setting getSetting() {
+		return setting;
+	}
+	
+	public void setSetting(Setting setting) {
+		this.setting = setting;
+	}
+	
+	public List<Category> getCategories() {
+	    return categories;
+	}
+	
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+	
+	public List<Transaction> getTransactions() {
+	    return transactions;
+	}
+	
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof User)) return false;
+		User other = (User) o;
+		return id != null && id.equals(other.getId());
+	}
+	
+	@Override
+	public int hashCode() {
+		return id == null ? 0 : id.hashCode();
+	}
+}
