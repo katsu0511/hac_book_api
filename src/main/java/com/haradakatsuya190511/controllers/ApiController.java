40db998d8449df66dea5bb82fd79a287227e1a64
Katsuya <haradakatsuya190511@gmail.com>
2025-08-30 19:11:58 -0700

    developed login controller

--- src/main/java/com/haradakatsuya190511/controllers/ApiController.java
+++ src/main/java/com/haradakatsuya190511/controllers/ApiController.java
@@ -1,13 +1,45 @@
-package com.haradakatsuya190511.controller;
+package com.haradakatsuya190511.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.haradakatsuya190511.dtos.LoginRequestDto;
+import com.haradakatsuya190511.entities.User;
+import com.haradakatsuya190511.repositories.UserRepository;
+import com.haradakatsuya190511.services.AuthService;
+import com.haradakatsuya190511.utils.JwtUtil;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 @RestController
 public class ApiController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello World!";
+	@Autowired
+	UserRepository repository;
+
+	@Autowired
+	AuthService authService;
+
+	@Autowired
+	JwtUtil jwtUtil;
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDto loginUser, HttpServletResponse response) {
+		User user = authService.authenticate(loginUser.getEmail(), loginUser.getPassword());
+		String jwt = jwtUtil.generateToken(user);
+		Cookie cookie = new Cookie("token", jwt);
+		cookie.setHttpOnly(true);
+//		cookie.setSecure(true);
+		cookie.setPath("/");
+		cookie.setMaxAge(3600);
+		response.addCookie(cookie);
+		return ResponseEntity.ok(Map.of("message", "succeeded to login"));
 	}
 }
