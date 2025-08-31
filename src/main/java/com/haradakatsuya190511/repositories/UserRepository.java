edcd8f6dc08a5ce951641f063854eec3e42e75bb
Katsuya <haradakatsuya190511@gmail.com>
2025-08-30 19:17:13 -0700

    created UserRepository

---
+++ src/main/java/com/haradakatsuya190511/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.haradakatsuya190511.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.haradakatsuya190511.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	public Optional<User> findByEmail(String email);
+}
