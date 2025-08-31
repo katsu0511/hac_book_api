16f16f487bc1c52813a169cfa538a9a48f87ec7f
Katsuya <haradakatsuya190511@gmail.com>
2025-08-30 20:42:37 -0700

    integrate authentication function to AuthController.java

--- src/main/java/com/haradakatsuya190511/controllers/AuthController.java
+++ src/main/java/com/haradakatsuya190511/controllers/AuthController.java
@@ -6,22 +6,49 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.haradakatsuya190511.dtos.LoginRequestDto;
+import com.haradakatsuya190511.entities.User;
+import com.haradakatsuya190511.services.AuthService;
 import com.haradakatsuya190511.services.JwtService;
 import com.haradakatsuya190511.services.TokenService;
+import com.haradakatsuya190511.utils.JwtUtil;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 @RestController
 public class AuthController {
+	
+	@Autowired
+	AuthService authService;
+
+	@Autowired
+	JwtUtil jwtUtil;
 	
 	@Autowired
 	TokenService tokenService;
 	
 	@Autowired
 	JwtService jwtService;
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDto loginUser, HttpServletResponse response) {
+		User user = authService.authenticate(loginUser.getEmail(), loginUser.getPassword());
+		String jwt = jwtUtil.generateToken(user);
+		Cookie cookie = new Cookie("token", jwt);
+		cookie.setHttpOnly(true);
+//		cookie.setSecure(true);
+		cookie.setPath("/");
+		cookie.setMaxAge(3600);
+		response.addCookie(cookie);
+		return ResponseEntity.ok(Map.of("message", "succeeded to login"));
+	}
 	
 	@GetMapping("/check-auth")
 	public ResponseEntity<Map<String, Boolean>> checkAuth(HttpServletRequest request, HttpServletResponse response) {
