22e5e684e38a942f419e1faa4341aee93cf45bde
Katsuya <haradakatsuya190511@gmail.com>
2025-09-01 14:21:59 -0700

    apply filter to security config

--- src/main/java/com/haradakatsuya190511/config/SecurityConfig.java
+++ src/main/java/com/haradakatsuya190511/config/SecurityConfig.java
@@ -7,9 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.haradakatsuya190511.filters.JwtAuthenticationFilter;
+
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -20,16 +24,20 @@ public class SecurityConfig {
 	@Autowired
 	CorsProperties corsProperties;
 	
+	@Autowired
+	JwtAuthenticationFilter jwtAuthenticationFilter;
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/login", "/signup", "/check-auth", "/logout").permitAll()
+				.requestMatchers("/login", "/signup", "/check-auth").permitAll()
 				.anyRequest().authenticated()
 			)
 			.csrf(csrf -> csrf.disable())
 			.cors(Customizer.withDefaults())
-			.logout(logout -> logout.disable());
+			.logout(logout -> logout.disable())
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 	
