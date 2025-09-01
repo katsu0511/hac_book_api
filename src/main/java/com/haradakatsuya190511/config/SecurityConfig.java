90a43546e1ac172d425768772ec21d5458a0b5fa
Katsuya <haradakatsuya190511@gmail.com>
2025-08-31 18:05:03 -0700

    fix security settings

--- src/main/java/com/haradakatsuya190511/config/SecurityConfig.java
+++ src/main/java/com/haradakatsuya190511/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -25,14 +24,12 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/login", "/signup", "/check-auth").permitAll()
+				.requestMatchers("/login", "/signup", "/check-auth", "/logout").permitAll()
 				.anyRequest().authenticated()
 			)
-			.csrf(csrf -> csrf
-				.ignoringRequestMatchers("/login", "/signup", "/check-auth")
-				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-			)
-			.cors(Customizer.withDefaults());
+			.csrf(csrf -> csrf.disable())
+			.cors(Customizer.withDefaults())
+			.logout(logout -> logout.disable());
 		return http.build();
 	}
 	
