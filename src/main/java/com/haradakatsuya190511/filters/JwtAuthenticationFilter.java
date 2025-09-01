baf007c9fc15dc80409bf36caf8c98576d2fee00
Katsuya <haradakatsuya190511@gmail.com>
2025-09-01 14:20:50 -0700

    modified for user authentication

--- src/main/java/com/haradakatsuya190511/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/haradakatsuya190511/filters/JwtAuthenticationFilter.java
@@ -3,52 +3,56 @@ package com.haradakatsuya190511.filters;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.haradakatsuya190511.services.JwtService;
 import com.haradakatsuya190511.services.TokenService;
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebFilter("/api/*")
-public class JwtAuthenticationFilter implements Filter {
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	
 	@Autowired
-	TokenService tokenService;
+	private TokenService tokenService;
 	
 	@Autowired
-	JwtService jwtService;
+	private JwtService jwtService;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 	
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		HttpServletResponse httpResponse = (HttpServletResponse) response;
-		Cookie tokenCookie = tokenService.getTokenCookie(httpRequest);
-		
+		Cookie tokenCookie = tokenService.getTokenCookie(request);
+		String token = null;
 		if (tokenCookie != null) {
-			String token = tokenCookie.getValue();
-			try {
-				jwtService.checkJwts(token, httpResponse);
-				chain.doFilter(request, response);
-			} catch (Exception e) {
-				httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
-			}
-		} else {
-			httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token is missing");
+			token = tokenCookie.getValue();
 		}
+		
+		try {
+			if (token != null && jwtService.validateToken(token)) {
+				String email = jwtService.extractEmail(token);
+				UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid JWT token");
+			return;
+		}
+		
+		filterChain.doFilter(request, response);
 	}
-	
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {}
-	
-	@Override
-	public void destroy() {}
 }
