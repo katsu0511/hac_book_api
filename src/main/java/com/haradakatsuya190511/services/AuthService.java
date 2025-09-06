1185340751d1c3f4b5c708f2afb3f3bfc733a0d5
Katsuya <haradakatsuya190511@gmail.com>
2025-09-06 11:05:14 -0700

    developed login service

--- src/main/java/com/haradakatsuya190511/services/AuthService.java
+++ src/main/java/com/haradakatsuya190511/services/AuthService.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Service;
 import com.haradakatsuya190511.entities.User;
 import com.haradakatsuya190511.exceptions.LoginFailedException;
 import com.haradakatsuya190511.repositories.UserRepository;
+import com.haradakatsuya190511.utils.JwtUtil;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Service
 public class AuthService {
@@ -17,9 +21,22 @@ public class AuthService {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
+	@Autowired
+	JwtUtil jwtUtil;
+	
 	public User authenticate(String email, String password) {
 		return repository.findByEmail(email)
 				.filter(user -> passwordEncoder.matches(password, user.getPassword()))
 				.orElseThrow(LoginFailedException::new);
     }
+	
+	public void login(HttpServletResponse response, User user) {
+		String jwt = jwtUtil.generateToken(user);
+		Cookie cookie = new Cookie("token", jwt);
+		cookie.setHttpOnly(true);
+//		cookie.setSecure(true);
+		cookie.setPath("/");
+		cookie.setMaxAge(3600);
+		response.addCookie(cookie);
+	}
 }
