735d69dc430a6a7ed37ec43a0ad21356a4be18b5
Katsuya <haradakatsuya190511@gmail.com>
2025-09-07 12:45:15 -0700

    developed signup end point

--- src/main/java/com/haradakatsuya190511/controllers/AuthController.java
+++ src/main/java/com/haradakatsuya190511/controllers/AuthController.java
@@ -7,13 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.haradakatsuya190511.dtos.LoginRequestDto;
+import com.haradakatsuya190511.dtos.SignupRequestDto;
+import com.haradakatsuya190511.entities.Setting;
 import com.haradakatsuya190511.entities.User;
+import com.haradakatsuya190511.repositories.SettingRepository;
+import com.haradakatsuya190511.repositories.UserRepository;
 import com.haradakatsuya190511.services.AuthService;
 import com.haradakatsuya190511.services.TokenService;
 
@@ -30,6 +35,15 @@ public class AuthController {
 	
 	@Autowired
 	TokenService tokenService;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	SettingRepository settingRepository;
 	
 	@PostMapping("/login")
 	public ResponseEntity<Map<String, String>> login(@Valid @RequestBody LoginRequestDto loginUser, HttpServletResponse response) {
@@ -52,6 +66,17 @@ public class AuthController {
 		}
 		
 		return ResponseEntity.ok(Map.of("message", "Succeeded to logout"));
+	}
+	
+	@PostMapping("/signup")
+	public ResponseEntity<Map<String, String>> signup(@Valid @RequestBody SignupRequestDto signupUser, HttpServletResponse response) {
+		authService.checkEmailNotExists(signupUser.getEmail());
+		String hashedPassword = passwordEncoder.encode(signupUser.getPassword());
+		User user = new User(signupUser.getName(), signupUser.getEmail(), hashedPassword);
+		userRepository.save(user);
+		settingRepository.save(new Setting(user.getId()));
+		authService.login(response, user);
+		return ResponseEntity.ok(Map.of("message", "succeeded to signup"));
 	}
 	
 	@GetMapping("/check-auth")
