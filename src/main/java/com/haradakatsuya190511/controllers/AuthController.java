5cce6927f7e13b05b77d1fdc755f0536da49af45
Katsuya <haradakatsuya190511@gmail.com>
2025-09-08 21:38:23 -0700

    use signup method in AuthService

--- src/main/java/com/haradakatsuya190511/controllers/AuthController.java
+++ src/main/java/com/haradakatsuya190511/controllers/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.haradakatsuya190511.dtos.LoginRequestDto;
 import com.haradakatsuya190511.dtos.SignupRequestDto;
-import com.haradakatsuya190511.entities.Setting;
 import com.haradakatsuya190511.entities.User;
-import com.haradakatsuya190511.repositories.SettingRepository;
-import com.haradakatsuya190511.repositories.UserRepository;
 import com.haradakatsuya190511.services.AuthService;
 import com.haradakatsuya190511.services.TokenService;
 
@@ -35,15 +31,6 @@ public class AuthController {
 	
 	@Autowired
 	TokenService tokenService;
-	
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	
-	@Autowired
-	UserRepository userRepository;
-	
-	@Autowired
-	SettingRepository settingRepository;
 	
 	@PostMapping("/login")
 	public ResponseEntity<Map<String, String>> login(@Valid @RequestBody LoginRequestDto loginUser, HttpServletResponse response) {
@@ -70,11 +57,7 @@ public class AuthController {
 	
 	@PostMapping("/signup")
 	public ResponseEntity<Map<String, String>> signup(@Valid @RequestBody SignupRequestDto signupUser, HttpServletResponse response) {
-		authService.checkEmailNotExists(signupUser.getEmail());
-		String hashedPassword = passwordEncoder.encode(signupUser.getPassword());
-		User user = new User(signupUser.getName(), signupUser.getEmail(), hashedPassword);
-		userRepository.save(user);
-		settingRepository.save(new Setting(user.getId()));
+		User user = authService.signup(signupUser);
 		authService.login(response, user);
 		return ResponseEntity.ok(Map.of("message", "succeeded to signup"));
 	}
