f6f28f687f71296c8d97f56b2cf0750b4970ec19
Katsuya <haradakatsuya190511@gmail.com>
2025-09-11 16:20:15 -0700

    updated User entity

--- src/main/java/com/haradakatsuya190511/entities/User.java
+++ src/main/java/com/haradakatsuya190511/entities/User.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -39,6 +40,8 @@ public class User {
 	private String email;
 	
 	@Column(length = 255, nullable = false)
+	@NotBlank
+	@JsonIgnore
 	private String password;
 	
 	@Column(insertable = false, updatable = false)
@@ -47,11 +50,11 @@ public class User {
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private Setting setting;
 	
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
 	@JsonIgnore
 	private List<Category> categories = new ArrayList<>();
 	
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
 	@JsonIgnore
 	private List<Transaction> transactions = new ArrayList<>();
 	
