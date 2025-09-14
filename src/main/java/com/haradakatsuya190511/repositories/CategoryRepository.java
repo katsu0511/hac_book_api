84a7c42d11dff93941898d04b7b6c54005e1e0a9
Katsuya <haradakatsuya190511@gmail.com>
2025-09-14 10:37:17 -0700

    created CategoryRepository

---
+++ src/main/java/com/haradakatsuya190511/repositories/CategoryRepository.java
@@ -0,0 +1,21 @@
+package com.haradakatsuya190511.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.haradakatsuya190511.entities.Category;
+import com.haradakatsuya190511.entities.User;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	@Query("SELECT c FROM Category c WHERE (c.user = :user OR c.user IS NULL) AND c.type = 'INCOME'")
+	List<Category> findIncomeByUserOrDefault(@Param("user") User user);
+	
+	@Query("SELECT c FROM Category c WHERE (c.user = :user OR c.user IS NULL) AND c.type = 'EXPENSE'")
+	List<Category> findExpenseByUserOrDefault(@Param("user") User user);
+}
