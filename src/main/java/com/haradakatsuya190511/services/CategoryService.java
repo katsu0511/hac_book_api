594ee2716b5cc55d1850cf41fd479db1d61a9a09
Katsuya <haradakatsuya190511@gmail.com>
2025-09-17 15:14:12 -0700

    created service for category

---
+++ src/main/java/com/haradakatsuya190511/services/CategoryService.java
@@ -0,0 +1,37 @@
+package com.haradakatsuya190511.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.haradakatsuya190511.dtos.AddCategoryRequestDto;
+import com.haradakatsuya190511.entities.Category;
+import com.haradakatsuya190511.entities.User;
+import com.haradakatsuya190511.enums.CategoryType;
+import com.haradakatsuya190511.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	public List<Category> getIncomeCategories(User user) {
+		return categoryRepository.findIncomeByUserOrDefault(user);
+	}
+	
+	public List<Category> getExpenseCategories(User user) {
+		return categoryRepository.findExpenseByUserOrDefault(user);
+	}
+	
+	public Category addCategory(User user, AddCategoryRequestDto request) {
+		Long parentId = request.getParentId();
+		Category parentCategory = categoryRepository.findById(parentId).orElse(null);
+		String name = request.getName();
+		CategoryType type = request.getType();
+		String description = request.getDescription();
+		Category category = new Category(user, parentCategory, name, type, description);
+		return category;
+	}
+}
