0524f259e93f409650377bd0c0a779666f81e442
Katsuya <haradakatsuya190511@gmail.com>
2025-09-21 13:05:36 -0700

    created DTO for category modify request

---
+++ src/main/java/com/haradakatsuya190511/dtos/ModifyCategoryRequestDto.java
@@ -0,0 +1,52 @@
+package com.haradakatsuya190511.dtos;
+
+import com.haradakatsuya190511.dtos.shared.CategoryRequest;
+import com.haradakatsuya190511.enums.CategoryType;
+
+public class ModifyCategoryRequestDto implements CategoryRequest {
+	private Long id;
+	private Long parentId;
+	private String name;
+	private CategoryType type;
+	private String description;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getParentId() {
+		return parentId;
+	}
+	
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public CategoryType getType() {
+		return type;
+	}
+	
+	public void setType(CategoryType type) {
+		this.type = type;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
