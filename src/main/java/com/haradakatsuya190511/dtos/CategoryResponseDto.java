6ec1d2e3076b26583070edc994adb5ded593b6e8
Katsuya <haradakatsuya190511@gmail.com>
2025-09-22 11:32:18 -0700

    created DTO for Category Response

---
+++ src/main/java/com/haradakatsuya190511/dtos/CategoryResponseDto.java
@@ -0,0 +1,52 @@
+package com.haradakatsuya190511.dtos;
+
+import com.haradakatsuya190511.entities.Category;
+import com.haradakatsuya190511.enums.CategoryType;
+
+public class CategoryResponseDto {
+	private Long id;
+	private Long userId;
+	private Long parentId;
+	private String name;
+	private CategoryType type;
+	private String description;
+	private boolean isActive;
+	
+	public CategoryResponseDto(Category category) {
+		this.id = category.getId();
+		this.userId = category.getUser() == null ? null : category.getUser().getId();
+		this.parentId = category.getParentCategory() == null ? null : category.getParentCategory().getId();
+		this.name = category.getName();
+		this.type = category.getType();
+		this.description = category.getDescription();
+		this.isActive = category.isActive();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public Long getUserId() {
+		return userId;
+	}
+	
+	public Long getParentId() {
+		return parentId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public CategoryType getType() {
+		return type;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public boolean isActive() {
+		return isActive;
+	}
+}
