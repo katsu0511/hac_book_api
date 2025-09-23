0952553fb1c56aa9bcfe653b4f7210bdeaed44e4
Katsuya <haradakatsuya190511@gmail.com>
2025-09-23 15:27:44 -0700

    created getCategory service

--- src/main/java/com/haradakatsuya190511/services/CategoryService.java
+++ src/main/java/com/haradakatsuya190511/services/CategoryService.java
@@ -10,6 +10,7 @@ import com.haradakatsuya190511.dtos.CategoryResponseDto;
 import com.haradakatsuya190511.dtos.shared.CategoryRequest;
 import com.haradakatsuya190511.entities.Category;
 import com.haradakatsuya190511.entities.User;
+import com.haradakatsuya190511.exceptions.CategoryNotFoundException;
 import com.haradakatsuya190511.repositories.CategoryRepository;
 
 @Service
@@ -32,6 +33,13 @@ public class CategoryService {
 	
 	public List<Category> getParentCategories(User user) {
 		return categoryRepository.findParentCategoriesByUserOrDefault(user);
+	}
+	
+	public CategoryResponseDto getCategory(User user, Long id) {
+		return categoryRepository.findById(id)
+			.filter(c -> c.getUser().getId().equals(user.getId()))
+			.map(CategoryResponseDto::new)
+			.orElseThrow(CategoryNotFoundException::new);
 	}
 	
 	public Category addCategory(User user, AddCategoryRequestDto request) {
