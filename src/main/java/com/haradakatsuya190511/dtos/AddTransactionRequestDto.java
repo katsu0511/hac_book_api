2a630c79962151a78dd57eaf9caa91891b0fb276
Katsuya <haradakatsuya190511@gmail.com>
2025-10-06 10:20:23 -0700

    new DTO
    created a new DTO for add request of transaction

---
+++ src/main/java/com/haradakatsuya190511/dtos/AddTransactionRequestDto.java
@@ -0,0 +1,61 @@
+package com.haradakatsuya190511.dtos;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class AddTransactionRequestDto {
+	private Long userId;
+	private Long categoryId;
+	private BigDecimal amount;
+	private String currency;
+	private String description;
+	private LocalDate transactionDate;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	
+	public Long getCategoryId() {
+		return categoryId;
+	}
+	
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+	
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	
+	public String getCurrency() {
+		return currency;
+	}
+	
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public LocalDate getTransactionDate() {
+		return transactionDate;
+	}
+	
+	public void setTransactionDate(LocalDate transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+}
