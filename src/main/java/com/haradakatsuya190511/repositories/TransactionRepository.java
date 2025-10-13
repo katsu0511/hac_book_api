72a9b08f01fc2093992237a38f78562bd73f68c7
Katsuya <haradakatsuya190511@gmail.com>
2025-10-12 23:05:06 -0700

    findById
    added a new function findById

--- src/main/java/com/haradakatsuya190511/repositories/TransactionRepository.java
+++ src/main/java/com/haradakatsuya190511/repositories/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.haradakatsuya190511.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import com.haradakatsuya190511.entities.User;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+	Optional<Transaction> findById(Long id);
 	List<Transaction> findByUser(User user);
 }
