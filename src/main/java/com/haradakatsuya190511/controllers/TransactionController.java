e42a099cedf13a07348c4f7d6fece402d14ce1e9
Katsuya <haradakatsuya190511@gmail.com>
2025-10-17 16:05:58 -0700

    new end point
    created a new API end point for update transaction data

--- src/main/java/com/haradakatsuya190511/controllers/TransactionController.java
+++ src/main/java/com/haradakatsuya190511/controllers/TransactionController.java
@@ -9,10 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.haradakatsuya190511.dtos.AddTransactionRequestDto;
+import com.haradakatsuya190511.dtos.ModifyTransactionRequestDto;
 import com.haradakatsuya190511.dtos.TransactionResponseDto;
 import com.haradakatsuya190511.entities.User;
 import com.haradakatsuya190511.services.AuthService;
@@ -43,5 +45,11 @@ public class TransactionController {
 	public ResponseEntity<TransactionResponseDto> createTransaction(Principal principal, @RequestBody AddTransactionRequestDto request) {
 		User user = authService.getUser(principal);
 		return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.createTransaction(user, request));
+	}
+	
+	@PutMapping("/transactions/{id}")
+	public ResponseEntity<TransactionResponseDto> updateTransaction(Principal principal, @PathVariable("id") Long id, @RequestBody ModifyTransactionRequestDto request) {
+		User user = authService.getUser(principal);
+		return ResponseEntity.ok(transactionService.updateTransaction(user, id, request));
 	}
 }
