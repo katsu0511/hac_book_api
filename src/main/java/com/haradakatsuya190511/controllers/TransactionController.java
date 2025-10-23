3696bb48e30cfb67c1572d19ab66050b990ba0a3
Katsuya <haradakatsuya190511@gmail.com>
2025-10-22 17:11:48 -0700

    new end point
    created a new end point for deleting a transaction

--- src/main/java/com/haradakatsuya190511/controllers/TransactionController.java
+++ src/main/java/com/haradakatsuya190511/controllers/TransactionController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,11 @@ public class TransactionController {
 	public ResponseEntity<TransactionResponseDto> updateTransaction(Principal principal, @PathVariable("id") Long id, @RequestBody ModifyTransactionRequestDto request) {
 		User user = authService.getUser(principal);
 		return ResponseEntity.ok(transactionService.updateTransaction(user, id, request));
+	}
+	
+	@DeleteMapping("/transactions/{id}")
+	public ResponseEntity<Boolean> deleteTransaction(Principal principal, @PathVariable("id") Long id) {
+		User user = authService.getUser(principal);
+		return ResponseEntity.ok(transactionService.deleteTransaction(user, id));
 	}
 }
