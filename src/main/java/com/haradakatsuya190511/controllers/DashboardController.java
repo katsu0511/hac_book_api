d4dc89f493b27cd9978be88d7dea44e9b79d44b2
Katsuya <haradakatsuya190511@gmail.com>
2025-12-01 12:06:14 -0800

    new Controller
    created a new Controller named DashboardController

---
+++ src/main/java/com/haradakatsuya190511/controllers/DashboardController.java
@@ -0,0 +1,42 @@
+package com.haradakatsuya190511.controllers;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.haradakatsuya190511.entities.User;
+import com.haradakatsuya190511.services.AuthService;
+import com.haradakatsuya190511.services.DashboardService;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController {
+	
+	@Autowired
+	AuthService authService;
+	
+	@Autowired
+	DashboardService dashboardService;
+	
+	@GetMapping("/summary")
+	public ResponseEntity<Map<String, String>> getSummary(
+			@RequestParam("start") LocalDate start,
+			@RequestParam("end") LocalDate end,
+			Principal principal
+	) {
+		User user = authService.getUser(principal);
+		return ResponseEntity.ok(
+			Map.of(
+				"income", dashboardService.getTotalIncome(user.getId(), start, end),
+				"expense", dashboardService.getTotalExpense(user.getId(), start, end)
+			)
+		);
+	}
+}
