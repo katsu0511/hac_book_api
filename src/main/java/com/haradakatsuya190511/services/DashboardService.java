03fdb35ce24448261d2c420fed09d6c853fd280a
Katsuya <haradakatsuya190511@gmail.com>
2025-12-15 07:40:50 -0800

    updated some dashboard services

--- src/main/java/com/haradakatsuya190511/services/DashboardService.java
+++ src/main/java/com/haradakatsuya190511/services/DashboardService.java
@@ -1,6 +1,8 @@
 package com.haradakatsuya190511.services;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,35 +23,38 @@ public class DashboardService {
 	@Autowired
 	CategoryService categoryService;
 	
-	public String getTotalIncome(User user, LocalDate start, LocalDate end) {
-		return transactionRepository.getTotalIncomeInPeriod(user, start, end);
-	}
-	
 	public String getTotalExpense(User user, LocalDate start, LocalDate end) {
 		return transactionRepository.getTotalExpenseInPeriod(user, start, end);
 	}
 	
-	public Map<String, String> getIncomeBreakdown(User user, LocalDate start, LocalDate end) {
-		List<CategoryResponseDto> categories = categoryService.getIncomeCategories(user);
-		
-		return categories.stream()
-			.collect(Collectors.toMap(
-				CategoryResponseDto::getName,
-				c -> transactionRepository.findSumByUserAndCategoryAndPeriod(
-					user, c.getId(), start, end
-				)
-			));
+	public String getTotalIncome(User user, LocalDate start, LocalDate end) {
+		return transactionRepository.getTotalIncomeInPeriod(user, start, end);
 	}
 	
 	public Map<String, String> getExpenseBreakdown(User user, LocalDate start, LocalDate end) {
-		List<CategoryResponseDto> categories = categoryService.getExpenseCategories(user);
-		
-		return categories.stream()
-			.collect(Collectors.toMap(
-				CategoryResponseDto::getName,
-				c -> transactionRepository.findSumByUserAndCategoryAndPeriod(
-					user, c.getId(), start, end
-				)
-			));
+		List<CategoryResponseDto> defaultCategories = categoryService.getDefaultExpenseCategories();
+		Map<String, String> map = getBreakdown(defaultCategories, user, start, end);
+		BigDecimal others = transactionRepository.getTotalExpenseByUserCategoryInPeriod(user, start, end);
+		BigDecimal existingOthers = new BigDecimal(map.getOrDefault("Others", "0.00"));
+		map.put("Others", existingOthers.add(others).toString());
+		return map;
+	}
+	
+	public Map<String, String> getIncomeBreakdown(User user, LocalDate start, LocalDate end) {
+		List<CategoryResponseDto> defaultCategories = categoryService.getDefaultIncomeCategories();
+		Map<String, String> map = getBreakdown(defaultCategories, user, start, end);
+		BigDecimal others = transactionRepository.getTotalIncomeByUserCategoryInPeriod(user, start, end);
+		BigDecimal existingOthers = new BigDecimal(map.getOrDefault("Others", "0.00"));
+		map.put("Others", existingOthers.add(others).toString());
+		return map;
+	}
+	
+	private Map<String, String> getBreakdown(List<CategoryResponseDto> categories, User user, LocalDate start, LocalDate end) {
+		return categories.stream().collect(Collectors.toMap(
+			CategoryResponseDto::getName,
+			c -> transactionRepository.findSumByCategoryAndPeriod(user, c.getId(), start, end),
+			(a, b) -> a,
+			LinkedHashMap::new
+		));
 	}
 }
