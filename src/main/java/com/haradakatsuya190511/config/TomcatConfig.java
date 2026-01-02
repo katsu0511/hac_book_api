1134cf6230f10da8a51fd66c92b1ec171d1bdfdd
Katsuya <haradakatsuya190511@gmail.com>
2026-01-01 16:52:02 -0800

    modified a bit

--- src/main/java/com/haradakatsuya190511/config/TomcatConfig.java
+++ src/main/java/com/haradakatsuya190511/config/TomcatConfig.java
@@ -14,7 +14,7 @@ public class TomcatConfig {
 		return factory -> factory.addContextCustomizers(context -> {
 			Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
 			cookieProcessor.setSameSiteCookies("Strict");
-//			cookieProcessor.setSameSiteCookies("Lax");
+//			cookieProcessor.setSameSiteCookies("None");
 			context.setCookieProcessor(cookieProcessor);
 		});
 	}
