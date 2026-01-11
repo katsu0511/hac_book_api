345e518ab05128fbff5e96050dbe5a1a649b0787
Katsuya <haradakatsuya190511@gmail.com>
2026-01-11 11:17:52 -0800

    modified a bit

--- src/main/java/com/haradakatsuya190511/services/CustomUserDetailsService.java
+++ src/main/java/com/haradakatsuya190511/services/CustomUserDetailsService.java
@@ -20,7 +20,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		User user = userRepository.findByEmail(email)
-			.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+			.orElseThrow(() -> new UsernameNotFoundException("User not found."));
 		
 		return org.springframework.security.core.userdetails.User
 			.withUsername(user.getEmail())
