f9477ad9d994cfb763eeccd6a2f56bd01e76baca
Katsuya <haradakatsuya190511@gmail.com>
2026-01-10 16:00:20 -0800

    modified auth end points

--- src/main/java/com/haradakatsuya190511/controllers/AuthController.java
+++ src/main/java/com/haradakatsuya190511/controllers/AuthController.java
@@ -1,61 +1,59 @@
 package com.haradakatsuya190511.controllers;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.haradakatsuya190511.dtos.LoginRequestDto;
-import com.haradakatsuya190511.dtos.SignupRequestDto;
-import com.haradakatsuya190511.dtos.UserResponseDto;
+import com.haradakatsuya190511.dtos.auth.LoginRequestDto;
+import com.haradakatsuya190511.dtos.auth.SignupRequestDto;
+import com.haradakatsuya190511.dtos.auth.UserResponseDto;
 import com.haradakatsuya190511.entities.User;
 import com.haradakatsuya190511.services.AuthService;
-import com.haradakatsuya190511.services.TokenService;
+import com.haradakatsuya190511.utils.AuthCookieManager;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 @RestController
 public class AuthController {
 	
-	@Autowired
-	AuthService authService;
+	private final AuthService authService;
+	private final AuthCookieManager cookieManager;
 	
-	@Autowired
-	TokenService tokenService;
+	public AuthController(AuthService authService, AuthCookieManager cookieManager) {
+		this.authService = authService;
+		this.cookieManager = cookieManager;
+	}
 	
 	@GetMapping("/check-auth")
-	public ResponseEntity<Map<String, Boolean>> checkAuth(HttpServletRequest request, HttpServletResponse response) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	public ResponseEntity<Boolean> checkAuth(Authentication authentication) {
 		boolean authenticated = authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken);
-		return ResponseEntity.ok(Map.of("authenticated", authenticated));
+		return ResponseEntity.ok(authenticated);
 	}
 	
 	@PostMapping("/login")
 	public ResponseEntity<UserResponseDto> login(@Valid @RequestBody LoginRequestDto loginUser, HttpServletResponse response) {
 		User user = authService.authenticate(loginUser.getEmail(), loginUser.getPassword());
-		authService.login(user, response);
+		String jwt = authService.generateToken(user);
+		cookieManager.setToken(response, jwt);
 		return ResponseEntity.ok(new UserResponseDto(user));
 	}
 	
 	@PostMapping("/logout")
 	public ResponseEntity<?> logout(HttpServletResponse response) {
-		authService.logout(response);
+		cookieManager.clearToken(response);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PostMapping("/signup")
 	public ResponseEntity<UserResponseDto> signup(@Valid @RequestBody SignupRequestDto signupUser, HttpServletResponse response) {
 		User user = authService.signup(signupUser);
-		authService.login(user, response);
+		String jwt = authService.generateToken(user);
+		cookieManager.setToken(response, jwt);
 		return ResponseEntity.ok(new UserResponseDto(user));
 	}
 }
