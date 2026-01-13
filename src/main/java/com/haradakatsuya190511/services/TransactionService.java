dcbfb6c5fc989f406f6a012e50e00d4b3a605a87
Katsuya <haradakatsuya190511@gmail.com>
2026-01-13 11:33:19 -0800

    updated TransactionService

--- src/main/java/com/haradakatsuya190511/services/TransactionService.java
+++ src/main/java/com/haradakatsuya190511/services/TransactionService.java
@@ -4,17 +4,17 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.haradakatsuya190511.dtos.transaction.AddTransactionRequestDto;
-import com.haradakatsuya190511.dtos.transaction.ModifyTransactionRequestDto;
+import com.haradakatsuya190511.dtos.transaction.CreateTransactionRequestDto;
+import com.haradakatsuya190511.dtos.transaction.UpdateTransactionRequestDto;
 import com.haradakatsuya190511.dtos.transaction.TransactionResponseDto;
 import com.haradakatsuya190511.dtos.transaction.shared.TransactionRequest;
 import com.haradakatsuya190511.entities.Category;
 import com.haradakatsuya190511.entities.Transaction;
 import com.haradakatsuya190511.entities.User;
 import com.haradakatsuya190511.exceptions.CategoryNotFoundException;
+import com.haradakatsuya190511.exceptions.InvalidCategoryException;
 import com.haradakatsuya190511.exceptions.TransactionNotFoundException;
 import com.haradakatsuya190511.repositories.CategoryRepository;
 import com.haradakatsuya190511.repositories.TransactionRepository;
@@ -22,11 +22,13 @@ import com.haradakatsuya190511.repositories.TransactionRepository;
 @Service
 public class TransactionService {
 	
-	@Autowired
-	TransactionRepository transactionRepository;
+	private final TransactionRepository transactionRepository;
+	private final CategoryRepository categoryRepository;
 	
-	@Autowired
-	CategoryRepository categoryRepository;
+	public TransactionService(TransactionRepository transactionRepository, CategoryRepository categoryRepository) {
+		this.transactionRepository = transactionRepository;
+		this.categoryRepository = categoryRepository;
+	}
 	
 	public List<TransactionResponseDto> getTransactionsInPeriod(User user, LocalDate start, LocalDate end) {
 		if (start == null || end == null) {
@@ -45,31 +47,33 @@ public class TransactionService {
 			.orElseThrow(TransactionNotFoundException::new);
 	}
 	
-	public TransactionResponseDto createTransaction(User user, AddTransactionRequestDto request) {
+	public TransactionResponseDto createTransaction(User user, CreateTransactionRequestDto request) {
 		Transaction transaction = new Transaction(user);
-		applyTransactionInfo(transaction, request);
+		applyTransactionInfo(transaction, user, request);
 		return new TransactionResponseDto(transactionRepository.save(transaction));
 	}
 	
-	public TransactionResponseDto updateTransaction(User user, Long id, ModifyTransactionRequestDto request) {
+	public TransactionResponseDto updateTransaction(User user, Long id, UpdateTransactionRequestDto request) {
 		Transaction transaction = transactionRepository.findByIdWithCategory(user, id)
 			.orElseThrow(TransactionNotFoundException::new);
-		applyTransactionInfo(transaction, request);
+		applyTransactionInfo(transaction, user, request);
 		TransactionResponseDto dto = new TransactionResponseDto(transaction);
 		transactionRepository.save(transaction);
 		return dto;
 	}
 	
-	public boolean deleteTransaction(User user, Long id) {
+	public void deleteTransaction(User user, Long id) {
 		Transaction transaction = transactionRepository.findByIdWithCategory(user, id)
 			.orElseThrow(TransactionNotFoundException::new);
 		transactionRepository.delete(transaction);
-		return true;
 	}
 	
-	private void applyTransactionInfo(Transaction transaction, TransactionRequest request) {
+	private void applyTransactionInfo(Transaction transaction, User user, TransactionRequest request) {
 		Long categoryId = request.getCategoryId();
 		Category category = categoryRepository.findById(categoryId).orElseThrow(CategoryNotFoundException::new);
+		boolean isDefault = category.getUser() == null;
+		boolean isUserOwned = category.getUser() != null && category.getUser().getId().equals(user.getId());
+		if (!isDefault && !isUserOwned) throw new InvalidCategoryException();
 		transaction.setCategory(category);
 		transaction.setAmount(request.getAmount());
 		transaction.setCurrency(request.getCurrency());
