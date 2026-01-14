627448693a94c48d2abe17ff9f3c7a9fee8afa45
Katsuya <haradakatsuya190511@gmail.com>
2026-01-14 12:23:28 -0800

    updated DashboardController

--- src/main/java/com/haradakatsuya190511/controllers/DashboardController.java
+++ src/main/java/com/haradakatsuya190511/controllers/DashboardController.java
@@ -2,16 +2,14 @@ package com.haradakatsuya190511.controllers;
 
 import java.security.Principal;
 import java.time.LocalDate;
-import java.time.YearMonth;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.haradakatsuya190511.dtos.summary.SummaryResponseDto;
 import com.haradakatsuya190511.entities.User;
 import com.haradakatsuya190511.services.AuthService;
 import com.haradakatsuya190511.services.DashboardService;
@@ -20,31 +18,21 @@ import com.haradakatsuya190511.services.DashboardService;
 @RequestMapping("/dashboard")
 public class DashboardController {
 	
-	@Autowired
-	AuthService authService;
+	private final AuthService authService;
+	private final DashboardService dashboardService;
 	
-	@Autowired
-	DashboardService dashboardService;
+	public DashboardController(AuthService authService, DashboardService dashboardService) {
+		this.authService = authService;
+		this.dashboardService = dashboardService;
+	}
 	
 	@GetMapping("/summary")
-	public ResponseEntity<Map<String, Object>> getSummary(
+	public ResponseEntity<SummaryResponseDto> getSummary(
+			Principal principal,
 			@RequestParam(value = "start", required = false) LocalDate start,
-			@RequestParam(value = "end", required = false) LocalDate end,
-			Principal principal
+			@RequestParam(value = "end", required = false) LocalDate end
 	) {
 		User user = authService.getUser(principal);
-		if (start == null || end == null) {
-			YearMonth thisMonth = YearMonth.now();
-			start = thisMonth.atDay(1);
-			end = thisMonth.atEndOfMonth();
-		}
-		return ResponseEntity.ok(
-			Map.of(
-				"expense", dashboardService.getTotalExpense(user, start, end),
-				"income", dashboardService.getTotalIncome(user, start, end),
-				"expenseBreakdown", dashboardService.getExpenseBreakdown(user, start, end),
-				"incomeBreakdown", dashboardService.getIncomeBreakdown(user, start, end)
-			)
-		);
+		return ResponseEntity.ok(dashboardService.getSummary(user, start, end));
 	}
 }
