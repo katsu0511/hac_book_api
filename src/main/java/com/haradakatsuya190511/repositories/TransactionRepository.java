56a34e2a3ba203c0de8791d44f97cde6e89ed20a
Katsuya <haradakatsuya190511@gmail.com>
2026-01-15 11:26:18 -0800

    updated TransactionRepository

--- src/main/java/com/haradakatsuya190511/repositories/TransactionRepository.java
+++ src/main/java/com/haradakatsuya190511/repositories/TransactionRepository.java
@@ -1,6 +1,5 @@
 package com.haradakatsuya190511.repositories;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -12,36 +11,9 @@ import org.springframework.stereotype.Repository;
 
 import com.haradakatsuya190511.entities.Transaction;
 import com.haradakatsuya190511.entities.User;
-import com.haradakatsuya190511.enums.CategoryType;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
-	@Query("""
-		SELECT COALESCE(SUM(t.amount), 0) FROM Transaction t
-		WHERE t.user = :user
-		AND t.category.type = :type
-		AND t.transactionDate BETWEEN :start AND :end
-	""")
-	BigDecimal findSumByCategoryTypeAndPeriod(
-		@Param("user") User user,
-		@Param("type") CategoryType type,
-		@Param("start") LocalDate start,
-		@Param("end") LocalDate end
-	);
-	
-	@Query("""
-		SELECT COALESCE(SUM(t.amount), 0) FROM Transaction t
-		WHERE t.user = :user
-		AND (t.category.id = :categoryId OR t.category.parentCategory.id = :categoryId)
-		AND t.transactionDate BETWEEN :start AND :end
-	""")
-	BigDecimal findSumByCategoryAndPeriod(
-		@Param("user") User user,
-		@Param("categoryId") Long categoryId,
-		@Param("start") LocalDate start,
-		@Param("end") LocalDate end
-	);
 	
 	@Query("""
 		SELECT t FROM Transaction t
