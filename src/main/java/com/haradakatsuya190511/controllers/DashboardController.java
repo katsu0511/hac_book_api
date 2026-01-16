a09ba84538cf6a2148ea48612f99bd8f7a3d8c91
Katsuya <haradakatsuya190511@gmail.com>
2026-01-16 13:15:39 -0800

    modified a bit
    stopped using Principal and used @AuthenticationPrincipal

--- src/main/java/com/haradakatsuya190511/controllers/DashboardController.java
+++ src/main/java/com/haradakatsuya190511/controllers/DashboardController.java
@@ -1,9 +1,9 @@
 package com.haradakatsuya190511.controllers;
 
-import java.security.Principal;
 import java.time.LocalDate;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,28 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.haradakatsuya190511.dtos.summary.SummaryResponseDto;
 import com.haradakatsuya190511.entities.User;
-import com.haradakatsuya190511.services.AuthService;
 import com.haradakatsuya190511.services.DashboardService;
 
 @RestController
 @RequestMapping("/dashboard")
 public class DashboardController {
 	
-	private final AuthService authService;
 	private final DashboardService dashboardService;
 	
-	public DashboardController(AuthService authService, DashboardService dashboardService) {
-		this.authService = authService;
+	public DashboardController(DashboardService dashboardService) {
 		this.dashboardService = dashboardService;
 	}
 	
 	@GetMapping("/summary")
 	public ResponseEntity<SummaryResponseDto> getSummary(
-			Principal principal,
-			@RequestParam(value = "start", required = false) LocalDate start,
-			@RequestParam(value = "end", required = false) LocalDate end
+		@AuthenticationPrincipal User user,
+		@RequestParam(value = "start", required = false) LocalDate start,
+		@RequestParam(value = "end", required = false) LocalDate end
 	) {
-		User user = authService.getUser(principal);
 		return ResponseEntity.ok(dashboardService.getSummary(user, start, end));
 	}
 }
