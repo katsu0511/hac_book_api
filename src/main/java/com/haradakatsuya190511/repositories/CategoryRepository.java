682075746fb947e74393a20f8752c257116c96e7
Katsuya <haradakatsuya190511@gmail.com>
2026-01-16 13:05:15 -0800

    modified a bit

--- src/main/java/com/haradakatsuya190511/repositories/CategoryRepository.java
+++ src/main/java/com/haradakatsuya190511/repositories/CategoryRepository.java
@@ -37,5 +37,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 	List<Category> findChildCategories(@Param("user") User user, @Param("type") CategoryType type);
 	
 	@EntityGraph(attributePaths = {"parentCategory"})
-	Optional<Category> findByIdWithParent(Long id);
+	Optional<Category> findWithParentById(Long id);
 }
