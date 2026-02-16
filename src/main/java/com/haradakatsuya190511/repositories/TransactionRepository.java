70a4ccce556aba2519c568bc4b0a565a7b2925e6
Katsuya <haradakatsuya190511@gmail.com>
2026-02-16 11:18:38 -0800

    modified along with updated db schema

--- src/main/java/com/haradakatsuya190511/repositories/TransactionRepository.java
+++ src/main/java/com/haradakatsuya190511/repositories/TransactionRepository.java
@@ -18,8 +18,9 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 	
 	@Query("""
 		SELECT t FROM Transaction t
-		JOIN FETCH t.category
+		JOIN FETCH t.category c
 		WHERE t.user = :user
+		AND c.user = :user
 		AND t.transactionDate BETWEEN :start AND :end
 	""")
 	List<Transaction> findAllWithCategoryInPeriod(
