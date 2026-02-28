0513227f72a74fd822c1b41d1ab74df11534c462
Katsuya <haradakatsuya190511@gmail.com>
2026-02-28 01:46:21 -0800

    updated test
    added Testcontainers and mockMVC

--- src/test/java/com/haradakatsuya190511/HacBookApiApplicationTests.java
+++ src/test/java/com/haradakatsuya190511/HacBookApiApplicationTests.java
@@ -1,10 +1,28 @@
 package com.haradakatsuya190511;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
 class HacBookApiApplicationTests {
+	
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18.2").withDatabaseName("test").withUsername("test").withPassword("test");
+	
+	@DynamicPropertySource
+	static void registerProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
