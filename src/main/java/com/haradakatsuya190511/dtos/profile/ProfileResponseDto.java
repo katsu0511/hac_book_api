084ae9a5a513bbecc684baf77f5de60820188862
Katsuya <haradakatsuya190511@gmail.com>
2026-05-06 11:14:41 -0700

    new DTO
    created a new DTO for response of profile

--- src/main/java/com/haradakatsuya190511/dtos/profile/ProfileResponseDto.java
+++ src/main/java/com/haradakatsuya190511/dtos/profile/ProfileResponseDto.java
@@ -1,36 +1,16 @@
 package com.haradakatsuya190511.dtos.profile;
 
-import com.haradakatsuya190511.entities.User;
-
 public class ProfileResponseDto {
-	private Long id;
-	private String name;
-	private String email;
-	private String icon;
+	private UserForProfileResponseDto user;
 	private SettingResponseDto setting;
 
-	public ProfileResponseDto(User user, SettingResponseDto setting) {
-		this.id = user.getId();
-		this.name = user.getName();
-		this.email = user.getEmail();
-		this.icon = user.getIcon();
+	public ProfileResponseDto(UserForProfileResponseDto user, SettingResponseDto setting) {
+		this.user = user;
 		this.setting = setting;
 	}
 	
-	public Long getId() {
-		return id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getIcon() {
-		return icon;
+	public UserForProfileResponseDto getUser() {
+		return user;
 	}
 	
 	public SettingResponseDto getSetting() {
