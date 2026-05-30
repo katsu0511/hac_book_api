e3cd7feaa64b9aff5774b0ba4cfdb77b1ba1d83e
Katsuya <haradakatsuya190511@gmail.com>
2026-05-29 23:05:15 -0700

    added updateName endpoint

--- src/main/java/com/haradakatsuya190511/controllers/ProfileController.java
+++ src/main/java/com/haradakatsuya190511/controllers/ProfileController.java
@@ -3,11 +3,16 @@ package com.haradakatsuya190511.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.haradakatsuya190511.dtos.profile.ProfileResponseDto;
+import com.haradakatsuya190511.dtos.profile.UpdateNameRequestDto;
 import com.haradakatsuya190511.entities.User;
 import com.haradakatsuya190511.services.ProfileService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProfileController {
@@ -21,5 +26,11 @@ public class ProfileController {
 	@GetMapping("/profile")
 	public ResponseEntity<ProfileResponseDto> getProfile(@AuthenticationPrincipal User user) {
 		return ResponseEntity.ok(new ProfileResponseDto(profileService.getUser(user), profileService.getSetting(user)));
+	}
+	
+	@PutMapping("/profile/name")
+	public ResponseEntity<Void> updateName(@AuthenticationPrincipal User user, @Valid @RequestBody UpdateNameRequestDto request) {
+		profileService.updateName(user.getId(), request.getName());
+		return ResponseEntity.noContent().build();
 	}
 }
